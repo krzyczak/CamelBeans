2db277b5fac1404c33748f3394149337325e051e
krzyczak <krzyczak@users.noreply.github.com>
2020-03-12 11:57:57 +0000

    working

--- src/main/java/io/github/krzyczak/App.java
+++ src/main/java/io/github/krzyczak/App.java
@@ -2,11 +2,15 @@ package io.github.krzyczak;
 
 import org.apache.camel.AggregationStrategy;
 import org.apache.camel.Exchange;
+import org.apache.camel.Expression;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class App
 {
+    static int count = 0;
+    
     public static void main( String[] args ) throws Exception {
         org.apache.camel.CamelContext context = new DefaultCamelContext();
 
@@ -16,11 +20,14 @@ public class App
             from("file://./?fileName=in.csv&noop=true")
               .split(body().tokenize("\n"))
               .filter().method(MyBean.class, "isGoldCustomer")
+                    .to("file://./gold/?fileName=${date:now:yyyy-MM-dd}_${exchangeId}.csv")
+                    .to("stream:out")
+              .end()
               .aggregate(constant(true), new AggregationStrategy() {
                   @Override
                   public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
                       if (oldExchange == null) {
-                          System.out.println("newExchange ----- " + newExchange.getIn().getBody());
+//                          System.out.println("newExchange ----- " + newExchange.getIn().getBody());
                           // we start a new correlation group, so complete all previous groups
   //                        newExchange.setProperty(Exchange.AGGREGATION_COMPLETE_ALL_GROUPS, true);
                           return newExchange;
@@ -29,13 +36,26 @@ public class App
                       String oldBody = oldExchange.getIn().getBody(String.class);
                       String newBody = newExchange.getIn().getBody(String.class);
 
-                      System.out.println("oldExchange ----- " + oldBody);
+                      System.out.println("oldExchange ----- " + oldBody + " filter-prop: " + newExchange.getProperty("CamelFilterMatched"));
 
-                      oldExchange.getIn().setBody(oldBody + "\n" + newBody);
+                      boolean exchangeWasFiltered = (boolean) newExchange.getProperty("CamelFilterMatched");
+                      if (exchangeWasFiltered) {
+                          oldExchange.getIn().setBody(oldBody);
+                      } else {
+                          oldExchange.getIn().setBody(oldBody + "\n" + newBody);
+                      }
+                      
                       return oldExchange;
                   }
               })
-              .completionInterval(10000)
+              .completionSize(exchangeProperty("CamelSplitSize"))
+//              .completionSize(MyBean.class, "completionSize")
+//              .completionSize(new Expression() {
+//                @Override
+//                public <T> T evaluate(Exchange exchange, Class<T> type) {
+//                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//                }
+//              })
 
               .to("file://./?fileName=out.csv")
               .end();
@@ -50,7 +70,7 @@ public class App
 
         try {
           System.out.println("Wating 2s...");
-          Thread.sleep(20000);
+          Thread.sleep(3000);
           System.out.println("Done Wating. Exiting.");
         } catch (InterruptedException e) {
           e.printStackTrace();
--- src/main/java/io/github/krzyczak/MyBean.java
+++ src/main/java/io/github/krzyczak/MyBean.java
@@ -2,11 +2,14 @@ package io.github.krzyczak;
 
 //import org.apache.camel.Header;
 
+import org.apache.camel.Exchange;
+
+
 public class MyBean {
 //    public static boolean isGoldCustomer(@Header("level") String level) {
     public static boolean isGoldCustomer(String level) {
         String[] data = level.split(",");
-        System.out.println("Level: " + level + " Gold: " + data[2] + " (" + data[2].equals("gold") + ").");
+//        System.out.println("Level: " + level + " Gold: " + data[2] + " (" + data[2].equals("gold") + ").");
         return data[2].equals("gold");
     }
-}
+   }
